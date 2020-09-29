505e548075fd4f221f75c98f79a161766f0411f0
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-09-29 17:22:37 -0400

    adding TDD tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,38 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { compareNumbers } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('This test should return 0 if the number guessed is correct.', (expect) => {
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+    const guess = 4;
+    const correctNumber = 4;
+    
+    const expected = 0;
+    const actual = compareNumbers(guess, correctNumber);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('This test should return -1 if the number guessed is too low.', (expect) => {
+    
+    const guess = 3;
+    const correctNumber = 4;
+    
+    const expected = -1;
+    const actual = compareNumbers(guess, correctNumber);
+
+    expect.equal(actual, expected);
+});
+
+test('This test should return 1 if the number guessed is too high.', (expect) => {
+    
+    const guess = 5;
+    const correctNumber = 4;
+    
+    const expected = 1;
+    const actual = compareNumbers(guess, correctNumber);
+
     expect.equal(actual, expected);
 });
