947b8a1e018ad329e9f7b1912f6997333f21b671
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-09-29 20:53:12 -0400

    fixed functionality

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// import functions and grab DOM elements
 import { compareNumbers } from './utils.js';
 
 const userInput = document.getElementById('user-input');
@@ -6,25 +5,28 @@ const startButton = document.getElementById('start-button');
 const guessesRemaining = document.getElementById('guesses-remaining');
 const winLoss = document.getElementById('win-loss');
 const messageBox = document.getElementById('message-box');
+const resetButton = document.getElementById('reset-button');
 
-const correctAnswer = Math.floor(Math.random() * 20);
+let correctAnswer = Math.floor(Math.random() * 20);
 
 let totalWin = 0;
 let totalGuesses = 4;
 
-// initialize state
 startButton.addEventListener('click', () => {
     const userNumber = Number(userInput.value);
     
     totalGuesses--;
-    
     winLoss.textContent = totalWin;
+ 
     guessesRemaining.textContent = totalGuesses;
+    console.log(totalGuesses, totalWin, correctAnswer);
     
     if (compareNumbers(userNumber, correctAnswer) === 0) {
-        totalWin++;
         messageBox.textContent = 'Congrats! You win!';
-        return;
+        totalWin++;
+        winLoss.textContent = totalWin;
+        totalGuesses = 4;
+        guessesRemaining.textContent = totalGuesses;
     } else if (compareNumbers(userNumber, correctAnswer) === -1) {
         messageBox.textContent = 'Your guess is too low. Please try again';
     } else if (compareNumbers(userNumber, correctAnswer) === 1) {
@@ -33,7 +35,12 @@ startButton.addEventListener('click', () => {
     
     if (totalGuesses === 0) {
         messageBox.textContent = 'Sorry. You\'re out of guesses. Better luck next time!';
+        startButton.disabled = true;
     }
 });
-// set event listeners to update state and DOM
-console.log(totalGuesses, totalWin);
+
+resetButton.addEventListener('click', () => {
+    correctAnswer = Math.floor(Math.random() * 20);
+    totalGuesses = 4;
+    guessesRemaining.textContent = totalGuesses;
+})
